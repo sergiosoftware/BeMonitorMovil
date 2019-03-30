6812ab63f3a641fc51d56d8b9c99643ed3cd328f
JulianCamiloLG <julycamy_125@hotmail.com>
2019-03-29 21:47:34 -0500

    Arreglo de alerts

--- src/app/cusolicitar-asesoria/cusolicitar-asesoria.page.ts
+++ src/app/cusolicitar-asesoria/cusolicitar-asesoria.page.ts
@@ -4,13 +4,17 @@ import {
 } from '@angular/core';
 
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {AlertController} from '@ionic/angular'
 import {AsesoriaService} from '../asesoria.service'
+import { from } from 'rxjs';
+
 
 @Component({
   selector: 'app-cusolicitar-asesoria',
   templateUrl: './cusolicitar-asesoria.page.html',
   styleUrls: ['./cusolicitar-asesoria.page.scss'],
 })
+
 
 export class CUSolicitarAsesoriaPage {
   /**
@@ -29,13 +33,39 @@ export class CUSolicitarAsesoriaPage {
       { type: 'pattern', message: 'Debe ingresar una asignatura para la asesoría' }
     ]
   }
-  constructor(public formBuilder: FormBuilder, public asesoriaService:AsesoriaService) {
+  constructor(public formBuilder: FormBuilder, public asesoriaService:AsesoriaService,public alertController:AlertController) {
     // Create the form and define fields and validators.
     this.form = this.formBuilder.group({
       asignaturaA: ['', Validators.pattern('[A-Z]+[0-9]+[A-Z]+[0-9]+')],
-      temaA: ['', Validators.pattern('[A-z0-9]+(\s*[A-z0-9]*)*[A-z0-9]')]
+      temaA: ['', Validators.pattern('[A-z0-9]+([A-z0-9 ]*)*[A-z0-9]')]
     });
   }
+  /**
+   * Método para generar el alter correcta
+   */
+  async alertCorrecta() {
+    const alert = await this.alertController.create({
+      header: 'Éxito',
+      message: 'Tu asesoría ha quedado registrada, pronto te responderá alguno de nuestros monitores',
+      buttons: ['Close']
+    });
+
+    await alert.present();
+  }
+
+  /**
+   * Método para generar el alter incorrecta
+   */
+  async alertIncorren() {
+    const alert = await this.alertController.create({
+      header: 'Nota',
+      message: 'No pudimos registrar tu asesoria, intentalo mas tarde',
+      buttons: ['Close']
+    });
+
+    await alert.present();
+  }
+
   /*
    * Save form values.
    */
@@ -50,9 +80,12 @@ export class CUSolicitarAsesoriaPage {
       let codigoEstudiante=1701310061;
       this.asesoriaService.addAsesoria(codigoEstudiante,this.asignaturaAsesoria,this.temaAsesoria).subscribe((data)=>{
         console.log(data);
+        this.alertCorrecta();
+        
       },
       (error=>{
         console.log(error);
+        this.alertIncorren();
       })
       )
     }
